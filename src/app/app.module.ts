97beaaa3378cff9bc2af97b8b2456fe9085ce7ae
andrur <andrur04@hotmail.com>
2019-05-10 11:35:28 -0300

    Fin seccion 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-//Rutas
+// Servicios
+import { HttpClientModule } from '@angular/common/http';
+
+// Rutas
 import { AppRoutingModule } from './app-routing.module';
 
-//Componentes
+// Componentes
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
+
 
 
 @NgModule({
@@ -24,7 +28,8 @@ import { ItemComponent } from './pages/item/item.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
